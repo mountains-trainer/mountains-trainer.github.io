943626c91b63ccb88d92d7a29f33c012c7abfcd7
mountains-trainer <51887643+mountains-trainer@users.noreply.github.com>
2019-06-16 21:16:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+    particlesJS.load('particles-js', '/js/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+});
